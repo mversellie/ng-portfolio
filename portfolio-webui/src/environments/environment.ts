a3bb115a59d583f1962befee1b4f43fe0ae6f180
marco <marco@versellie.com>
2024-07-17 14:49:48 -0400

    added base environment

---
+++ portfolio-webui/src/environments/environment.ts
@@ -0,0 +1,14 @@
+import examples from "./work-examples.json";
+
+const myname= "An Artist"
+
+export const environment = {
+    examples:examples["examples"],
+    gitUsername:"#myUsername",
+    navText:myname,
+    headerDescription:"Pretty Cool Guy",
+    headerName:myname,
+    author: myname,
+    navbarName: myname,
+    formSubmitUrl:"http://local:5000/api/form"
+};
